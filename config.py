afe61c3cefe933605624d2d384ba491e47ad9a0c
safi5890 <safikhan5890@gmail.com>
2024-09-20 21:42:22 -0700

    feature added

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+customhost = "employee.c32i0oo8of08.ap-south-1.rds.amazonaws.com"  # Example: "your-rds-endpoint.us-east-1.rds.amazonaws.com"
+customuser = "safi"
+custompass = "safikhan123"
+customdb = "employee"
+custombucket = "add-employee-project"
+customregion = "ap-south-1"  # Example: "us-east-1"
+
